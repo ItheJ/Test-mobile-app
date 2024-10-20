153b34feb5c9def6908048f65166511ce5b37527
ItheJ <146840197+ItheJ@users.noreply.github.com>
2024-10-20 22:35:18 +0500

    Update main.py
    Some moments of the program are censored

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ class Application(App):
     def click(self, instance):
         self.i += 1
         if self.i > 1:
-            self.lbl.text = f"Твою маму сбил камаз {self.i} раза! Хочешь ещё?"
+            self.lbl.text = f"Вы нажали {self.i} раза! Хочешь ещё?"
         else:
         
-            self.lbl.text = f"Твою маму сбил камаз {self.i} раз! Хочешь ещё?"
+            self.lbl.text = f"Вы нажали {self.i} раз! Хочешь ещё?"
 
     def click2(self, instance):
         self.i = 0
 
-        self.lbl.text = f"Ты спас маму от камаза!"
+        self.lbl.text = f"Обнуление!"
     
     def build(self):
 
         all_widgets = GridLayout(cols=1)
         self.i = 0
         
-        btn = Button(text="Нажми на меня, если твою маму сбил камаз", font_size=10, background_color="blue", on_press=self.click)
-        btn2 = Button(text="Нажми на меня, чтобы обнулить условие", font_size=20, background_color="blue", on_press=self.click2)
+        btn = Button(text="Нажми на меня", font_size=10, background_color="blue", on_press=self.click)
+        btn2 = Button(text="Обнулить условие", font_size=20, background_color="blue", on_press=self.click2)
 
         self.lbl = Label(text="       Выбери!", font_size = 20)
 
